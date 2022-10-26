b6515fbc385a93a8bdf11a4ce2b562679ba0eb6b
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-10-26 16:07:16 +0700

    Add array variable size 20, variable n and i

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,6 +8,16 @@ namespace SearchAlgorithm
 {
     internal class Program
     {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+        public void input()
+        {
+
+        }
         static void Main(string[] args)
         {
         }
